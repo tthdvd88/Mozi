00c7f97d60f94a60c3dec2ec4a8622f0701e6383
tthdvd88 <tthdvd88@gmail.com>
2024-10-22 17:36:47 +0200

    Alap applikáció

---
+++ src/test/java/com/example/mozi/MoziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
